45d278d4dd54fb1853adc04a9ee61ee781211397
NathaRuiz <nruizm98@gmail.com>
2023-12-11 14:40:33 +0100

    adjust responsive

--- src/assets/js/loading.js
+++ src/assets/js/loading.js
@@ -2,14 +2,14 @@ var percent = document.querySelector('.percent');
 var progress = document.querySelector('.progress');
 var count = 4;
 var per = 16;
-var loading = setInterval(animate, 50);
+var loading = setInterval(animate, 85);
 function animate() {
     if (count == 100 && per == 400) {
         percent.classList.add("text-blink");
         clearInterval(loading);
         setTimeout(() => {
             window.location.href="../pages/addNameList.html"            
-        }, 1000);
+        }, 1500);
     } else {
         per = per + 4;
         count = count + 1;
